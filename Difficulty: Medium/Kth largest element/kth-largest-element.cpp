c09fd7a3608b9f992c3ace136700aa3f66e1639a
Sana Perween <sana602perween@gmail.com>
2025-12-17 17:22:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth largest element/kth-largest-element.cpp
+++ Difficulty: Medium/Kth largest element/kth-largest-element.cpp
@@ -4,10 +4,15 @@ class Solution {
     int KthLargest(vector<int> &arr, int k) {
         // Your code here
         int n=arr.size();
-        sort(arr.begin(),arr.end());
         
-            return arr[n-k];
-        
+        priority_queue<int, vector<int>, greater<int>> p;
+       for(int i=0;i<n;i++)
+       {
+           p.push(arr[i]);
+           if(p.size()>k)
+           p.pop();
+       }
+        return p.top();
         
     }
 };
